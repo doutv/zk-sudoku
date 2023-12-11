b589de383b30c4f0185573a66fa5161b61c064d3
backdoor <backdoor705@proton.me>
2023-12-11 11:56:53 +0800

    fix relative path

--- frontend/src/zkproof/sudoku/snarkjsSudoku.js
+++ frontend/src/zkproof/sudoku/snarkjsSudoku.js
@@ -11,8 +11,8 @@ export async function sudokuCalldata(unsolved, solved) {
   try {
     dataResult = await exportCallDataGroth16(
       input,
-      "/zkproof/sudoku/sudoku.wasm",
-      "/zkproof/sudoku/sudoku_0001.zkey"
+      process.env.PUBLIC_URL + "/zkproof/sudoku/sudoku.wasm",
+      process.env.PUBLIC_URL + "/zkproof/sudoku/sudoku_0001.zkey"
     );
   } catch (error) {
     console.log(error);
